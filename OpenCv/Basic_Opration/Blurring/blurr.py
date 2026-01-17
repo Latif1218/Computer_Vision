2a3a9a4e149eae2b04a3bd37113a0dc78f15c39b
Latif1218 <mdsabdullotif@gmail.com>
2026-01-17 16:41:31 +0600

    uplode new file

---
+++ OpenCv/Basic_Opration/Blurring/blurr.py
@@ -0,0 +1,27 @@
+import os
+import cv2
+
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+img_path = os.path.join(BASE_DIR, "man.jpg")
+
+
+
+img = cv2.imread(img_path)
+
+if img is None:
+    raise FileNotFoundError(f"Image not found: {img_path}")
+
+
+k_size = 5
+img_blur = cv2.blur(img, (k_size, k_size))
+img_gaussian_blur = cv2.GaussianBlur(img, (k_size, k_size), 3)
+img_median_blur = cv2.medianBlur(img, k_size)
+
+print(img.shape)
+cv2.imshow('img', img)
+cv2.imshow('img1', img_blur)
+cv2.imshow('img2', img_gaussian_blur)
+cv2.imshow('img3', img_median_blur)
+cv2.waitKey(0)
--- OpenCv/Basic_Opration/RGB_ColorSpace/rgb_trans.py
+++ OpenCv/Basic_Opration/RGB_ColorSpace/rgb_trans.py
@@ -18,8 +18,12 @@ if img is None:
 crop_img = img[120:550, 180:800]
 
 img_rgb = cv2.cvtColor(crop_img, cv2.COLOR_BGR2RGB)
+img_gray = cv2.cvtColor(crop_img, cv2.COLOR_BGR2GRAY)
+img_hsv = cv2.cvtColor(crop_img, cv2.COLOR_BGR2HSV)
 print(crop_img.shape)
 # cv2.imshow('image1', img)
 cv2.imshow('img1', crop_img)
 cv2.imshow('img2', img_rgb)
+cv2.imshow('img3', img_gray)
+cv2.imshow('img4', img_hsv)
 cv2.waitKey(0)
